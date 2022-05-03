dae08806a42c86738e94a0c78a575713b5e7bd61
jaewoong2 <jwisgenius@naver.com>
2022-05-03 22:00:07 +0900

    :sparkles: implement TypoGraphy

---
+++ src/styles/typo.ts
@@ -0,0 +1,27 @@
+import { css } from "@emotion/react"
+
+export const TypoGraphy = {
+    Input: css`
+        font-size: 16px;
+        font-weight: 800;
+        font-family: 'Noto Sans KR', sans-serif;
+        line-height: calc(1ex / 0.32);
+        margin: calc(1ex / 0.32) 0;
+    `,
+    Title: css`
+        font-size: 20px;
+        font-weight: 800;
+        font-family: 'Noto Sans KR', sans-serif;
+        line-height: calc(1ex / 0.42);
+        margin: calc(1ex / 0.42) 0;
+    `,
+    Description: css`
+        font-size: 18px;
+        font-weight: 800;
+        font-family: 'Noto Sans KR', sans-serif;
+        line-height: calc(1ex / 0.38);
+        margin: calc(1ex / 0.38) 0;
+    `,
+}
+
+
