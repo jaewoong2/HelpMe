b809ac0e6cec023b28e520eec2498be8f5e8777f
jaewoong2 <jwisgenius@naver.com>
2022-05-03 22:41:23 +0900

    :sparkles: implement description

--- src/App.tsx
+++ src/App.tsx
@@ -2,50 +2,66 @@ import Button from '@components/Button';
 import Description from '@components/Description';
 import Input from '@components/Input';
 import Title from '@components/Title';
+import { css } from '@emotion/react';
 import React from 'react';
 
 type Props = {};
 const App = () => {
   return (
-    <div
+    <main
       style={{
         display: 'flex',
-        maxWidth: '1248px',
         width: '100%',
         height: '100%',
-        padding: '50px',
-        border: '1px solid green',
+        justifyContent: 'center',
+        alignItems: 'center',
       }}
     >
-      <div style={{ width: '50%', height: '100%' }}>
-        <Title>저는 이런 사람이에요</Title>
-        <Input title="이름" />
-        <Input title="이름" />
-        <Button>+ 추가하기</Button>
-        <Input title="이름" />
-        <Input title="이름" />
-        <Title>저는 이런 사람이에요</Title>
-        <Input title="이름" />
-        <Input title="이름" />
-        <Input title="이름" />
-        <Input title="이름" />
-        <Title>저는 이런 사람이에요</Title>
-        <Input title="이름" />
-        <Input title="이름" />
-        <Button>+ 추가하기</Button>
-        <Input title="이름" />
-        <Input title="이름" />
-        <Title>저는 이런 사람이에요</Title>
-        <Input title="이름" />
-        <Input title="이름" />
-        <Input title="이름" />
-        <Input title="이름" />
+      <div
+        style={{
+          marginTop: '120px',
+          display: 'flex',
+          maxWidth: '1248px',
+          width: '100%',
+          height: '100%',
+          maxHeight: '960px',
+          padding: '50px',
+          justifyContent: 'center',
+        }}
+      >
+        <div style={{ width: '50%', height: '100%' }}>
+          <Title>저는 이런 사람이에요</Title>
+          <Input title="이름" />
+          <Input title="이름" />
+          <Button>+ 추가하기</Button>
+          <Input title="이름" />
+          <Input title="이름" />
+          <Title>저는 이런 사람이에요</Title>
+          <Input title="이름" />
+          <Input title="이름" />
+          <Input title="이름" />
+          <Input title="이름" />
+        </div>
+        <section
+          style={{
+            maxHeight: '960px',
+            display: 'flex',
+            flexDirection: 'column',
+            width: '100%',
+            height: '100%',
+            marginLeft: '50px',
+          }}
+        >
+          <Title>Previw</Title>
+          <Description
+            css={css`
+              max-height: 70%;
+              height: calc(100% - 33px);
+            `}
+          />
+        </section>
       </div>
-      <section style={{ width: '100%', height: '100%', marginLeft: '50px' }}>
-        <Title>Previw</Title>
-        <Description />
-      </section>
-    </div>
+    </main>
   );
 };
 
--- src/components/Description/Description.styles.tsx
+++ src/components/Description/Description.styles.tsx
@@ -1,12 +1,14 @@
 import { SerializedStyles } from '@emotion/react';
 import styled from '@emotion/styled';
+import { TypoGraphy } from '@styles/typo';
 
 const Container = styled.div<{ css?: SerializedStyles }>`
   width: 100%;
   height: 100%;
   border-radius: 22px;
   background-color: ${({ theme }) => theme.color.textAreaBg};
-
+  padding: 10px 25px;
+  ${TypoGraphy.Description};
   ${({ css }) => css};
 `;
 
